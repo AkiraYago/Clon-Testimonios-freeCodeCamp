3e83779d7210ff29f7cba5b671334c1738fc178b
AkiraYago <the.programmer.yago@gmail.com>
2023-05-17 16:21:33 -0400

    strong freeCodeCamp word on the testimonials

--- src/components/Testimonio.js
+++ src/components/Testimonio.js
@@ -2,12 +2,26 @@ import React from "react";
 import "../style-sheets/Testimonio.css";
 
 function Testimonio(props) {
+  const textoTestimonio = props.testimonio;
+  const largoTexto = props.testimonio.length;
+  const palabraClave = "freeCodeCamp";
+  var negrearFrase = false;
+
+  var arrayTestimonio = textoTestimonio.split(" ");
+
+  for (let i = 0; i < arrayTestimonio.length; i++) {
+    if (arrayTestimonio[i] === palabraClave) {
+      console.log("Existe palabra clave");
+    }
+  }
+  var testimonioFinal = arrayTestimonio.map((n) => n === palabraClave ? (<strong>{n} </strong>):(n + " "));
+
   return (
     <div className="contenedor-testimonio">
       <img
         className="imagen-testimonio"
         src={require(`../img/testimonio-${props.imagen}.png`)}
-        alt="Foto de Emma"
+        alt={`Foto de ${props.nombre}`}
       />
       <div className="contenedor-texto-testimonio">
         <p className="nombre-testimonio">
@@ -17,7 +31,7 @@ function Testimonio(props) {
           {props.cargo} en <strong>{props.empresa}</strong>
         </p>
         <p className="texto-testimonio">
-          "{props.testimonio}"
+          "{testimonioFinal}"
         </p>
       </div>
     </div>
